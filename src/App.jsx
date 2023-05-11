624f50e1cd3aec64ae338f6b28194ffb59b51c78
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-05-11 20:08:43 +0300

    task 1 is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,17 @@
 import './App.css'
-import { getImageUrl } from './utils.js';
+import profiles from './profiles.js'
+import Profile from './Profile.jsx'
 
 export default function Gallery() {
-
-  const profiles = [
-    {
-      name: 'Maria Skłodowska-Curie',
-      imageId: 'szV5sdG',
-      profession: 'physicist and chemist',
-      discovered: 'polonium (element)',
-      awards: [
-        'Nobel Prize in Physics',
-        'Nobel Prize in Chemistry',
-        'Davy Medal',
-        'Matteucci Medal'
-      ]
-    },
-    {
-      name: 'Katsuko Saruhashi',
-      imageId: 'YfeOqp2',
-      profession: 'geochemist',
-      discovered: 'a method for measuring carbon dioxide in seawater',
-      awards: [
-        'Miyake Prize for geochemistry',
-        'Tanaka Prize'
-      ]
-    }
-  ]
 
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+
+      {profiles?.map((profile, index) => (
+        <Profile key={index} person={profile} />
+      ))}
+
     </div>
   );
 }
