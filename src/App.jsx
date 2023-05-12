fed6b58828b79f5f0ba9be92a04db14ebf407cc9
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-05-12 17:20:43 +0300

    task-3 (not) ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import './App.css'
-import { getImageUrl } from './utils.js';
 
-const Avatar = ({ person, size }) => (
-  <img
-    className="avatar"
-    src={getImageUrl(person, size < 90 ? 's' :'b')}
-    alt={person.name}
-    width={size}
-    height={size}
-  />
+const Card = ({ children }) => (
+  <div className="card">
+    <div className="card-content">
+      {children}
+    </div>
+  </div>
 )
 
 const Profile = () => (
-  <Avatar
-    size={40}
-    person={{
-      name: 'Gregorio Y. Zara',
-      imageId: '7vQD0fP'
-    }}
-  />
+  <div>
+    <Card>
+      <h1>Photo</h1>
+
+      <img
+        className="avatar"
+        src="https://i.imgur.com/OKS67lhm.jpg"
+        alt="Aklilu Lemma"
+        width={70}
+        height={70}
+      />
+    </Card>
+
+    <Card>
+      <h1>About</h1>
+
+      <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+    </Card>
+  </div>
 )
 
 export default Profile
